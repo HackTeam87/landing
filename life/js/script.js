1608cf3b97c7933aa7cbb6bcb3126f9ed1eb622f
YatskoGrigoriy <asusgrin@gmail.com>
2017-10-25 01:20:40 +0300

    update

---
+++ life/js/script.js
@@ -0,0 +1,18 @@
+// id="main"---- row-reverse
+function myFunction() {
+    document.getElementById("main").style.flexDirection = "row-reverse";
+}
+
+// scroll-display:none-.up-button
+var $btnTop = $(".up-button")
+$(window).on("scroll" ,function(){
+	if ($(window).scrollTop() >= 25){
+		$btnTop.fadeIn();
+	}else{
+		$btnTop.fadeOut();
+	}
+});
+// scroll-to-TOP:0 -- speed:5000
+$btnTop.on("click",function(){
+	$("html,body").animate({scrollTop:0},3000)
+});
