f84cc5b18f64462bf5b0bd5e6f5de6883fb483ab
YatskoGrigoriy <asusgrin@gmail.com>
2017-10-31 03:02:12 +0200

    update

---
+++ rabota.ua/js/script.js
@@ -0,0 +1,13 @@
+// scroll-display:none-.up-button
+var $btnTop = $(".up-button")
+$(window).on("scroll" ,function(){
+    if ($(window).scrollTop() >= 25){
+        $btnTop.fadeIn();
+    }else{
+        $btnTop.fadeOut();
+    }
+});
+// scroll-to-TOP:0 -- speed:5000
+$btnTop.on("click",function(){
+    $("html,body").animate({scrollTop:0},3000)
+});
